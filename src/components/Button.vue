a90449b8c7e491c8000f28c89a36381085151ec8
codernami <codernami@gmail.com>
2023-02-27 13:45:00 -0300

    Adding files

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button 
+    class="btn"
+    :style= "{background: color}"
+    @click= "$emit('toggle-add-task')" >
+        {{ text }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String
+    }
+}
+</script>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :key= "task.id" v-for= "task in tasks">
+        <TaskListItem 
+        :task= "task" 
+        @delete-task= "$emit('delete-task', task.id)"
+        @toggle-reminder= "$emit('toggle-reminder', task.id)"
+        />
+    </div>
+</template>
+
+<script>
+import TaskListItem from './TaskListItem'
+
+export default {
+    name: 'TaskList',
+    props: {
+        tasks: Array
+    },
+    components: {
+        TaskListItem 
+    }, 
+    emits: ['delete-task', 'toggle-reminder']
+}
+</script>
